baa6d72b94284a97a3d1c654b8f5f60dc4c222bd
BYAsurim <Pers0naL276276@gmail.com>
2023-08-13 10:08:41 +0300

    added img

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -9,6 +9,7 @@ import { UserPropsType} from "../../../redax/state";
 export const DialogItem = (props: UserPropsType) => {
     return (
         <div className={s.dialog + ' ' + s.active}>
+            <img className={s.img} alt='img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXEcE75-bcRjGyl0cytaeO-wBIGizyfH5dow&usqp=CAU'/>
             <NavLink to={'/dialogs/' + props.id}> {props.name} </NavLink>
         </div>
     )
