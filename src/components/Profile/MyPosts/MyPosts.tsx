585e2c0d867ab26d503017d5353e1f22aef2935f
BYAsurim <Pers0naL276276@gmail.com>
2023-08-22 12:55:06 +0300

    added addPost logic

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,43 +1,42 @@
 import React from 'react';
 import s from "./MyPosts.module.css";
 import Post from "../Posts/Post";
-import { PostPropsTypeArray} from "../../../redax/state";
-
+import {PostPropsTypeArray} from "../../../redax/state";
 
 
 type MyPostsPropsType = PostPropsTypeArray
 
 
+const MyPosts = (props: MyPostsPropsType) => {
 
-const MyPosts = (props:MyPostsPropsType) => {
-    // const posts = [
-    //     { id: v1(), post: "Сегодня замечательный день!", likecount: 10 },
-    //     { id: v1(), post: "Наконец-то закончил проект!", likecount: 25 },
-    //     { id: v1(), post: "Как же я люблю путешествовать!", likecount: 15 },
-    //     { id: v1(), post: "Вчера был на концерте своей любимой группы!", likecount: 30 },
-    //     { id: v1(), post: "Новый курс по JavaScript на IT-KAMASUTRA просто потрясающий!", likecount: 20 }
-    // ];
+    const newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    const addPost = () => {
+        if (newPostElement.current) {
+            let text = newPostElement.current.value
+            alert(text)
+        }
+    }
 
     return (
         <div>
             <div className={s.postsBlock}>
-               My posts
+                My posts
             </div>
-            <input value={'taps message'} className={s.input}/>
+            <textarea ref={newPostElement} className={s.input}></textarea>
+
             <div className={s.button}>
-            <button>Add post</button>
+                <button onClick={addPost}>Add post</button>
             </div>
             <div className={s.posts}>
                 {
-                    props.posts.map((el)=>{
-                     return(
-                         <Post id={el.id} post={el.post} likecount={el.likecount}/>
-                     )
+                    props.posts.map((el) => {
+                        return (
+                            <Post id={el.id} post={el.post} likecount={el.likecount}/>
+                        )
                     })
                 }
-            {/*// <Post message={'I like it!'} likeCounts={30}/>*/}
-            {/*// <Post message={'This is my first post'} likeCounts={10}/>*/}
-            {/*// <Post message={'Amazing! ! !'} likeCounts={11}/>*/}
+
             </div>
         </div>
     );
