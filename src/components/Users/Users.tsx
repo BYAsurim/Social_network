3b41caf707ae555e17d9c37a5e916a3941db8df7
BYAsurim <Pers0naL276276@gmail.com>
2023-10-19 20:20:25 +0300

    added style for Users.tsx component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,13 +1,121 @@
 import React from 'react';
 import {UsersPropsType} from "./UsersConteiner";
-
+import s from './users.module.css'
+import {v1} from "uuid";
 
 
 const Users: React.FC<UsersPropsType> = ({users, follow, unFollow, setUsers}) => {
+
+    if (users.length === 0) {
+        setUsers(
+            [
+                {
+                    id: v1(),
+                    followed: true,
+                    fullName: 'Alexander',
+                    status: 'I am very well',
+                    location: {city: 'Vileyka', country: 'Belarus'},
+                },
+                {
+                    id: v1(),
+                    followed: true,
+                    fullName: 'John',
+                    status: 'Feeling great!',
+                    location: {city: 'New York', country: 'United States'}
+                },
+                {
+                    id: v1(),
+                    followed: true,
+                    fullName: 'Emma',
+                    status: 'Enjoying life',
+                    location: {city: 'London', country: 'United Kingdom'}
+                },
+                {
+                    id: v1(),
+                    followed: true,
+                    fullName: 'Luisa',
+                    status: 'Feeling positive',
+                    location: {city: 'Paris', country: 'France'}
+                },
+                {
+                    id: v1(),
+                    followed: false,
+                    fullName: 'Mohammed',
+                    status: 'Excited for the future',
+                    location: {city: 'Dubai', country: 'United Arab Emirates'}
+                },
+                {
+                    id: v1(),
+                    followed: false,
+                    fullName: 'Sofia',
+                    status: 'Grateful for everything',
+                    location: {city: 'Rome', country: 'Italy'}
+                },
+                {
+                    id: v1(),
+                    followed: false,
+                    fullName: 'Hiroshi',
+                    status: 'Energized and motivated',
+                    location: {city: 'Tokyo', country: 'Japan'}
+                },
+                {
+                    id: v1(),
+                    followed: true,
+                    fullName: 'Elena',
+                    status: 'Finding joy in little things',
+                    location: {city: 'Moscow', country: 'Russia'}
+                }
+            ]
+        )
+    }
+
+
+    const unFollowHandler = (id: string) => {
+
+        unFollow(id)
+
+    }
+    const FollowHandler = (id: string) => {
+        follow(id)
+    }
+
+
     return (
         <div>
-            Users page
-            {}
+            <h3 className={s.title}>Users:</h3>
+
+            {users.map(u => <div key={u.id} className={s.wrapper}>
+                <div className={s.imgAndButton}>
+                    <div>
+                        <img className={s.photo} src="http://avotarov.ru/picture/avatar-80/kartinki/719.jpg" alt="img"/>
+                    </div>
+                    <div>
+                        {
+                            u.followed ?
+                                <button onClick={() => {
+                                    unFollowHandler(u.id)
+                                }}>UnFollow</button> :
+                                <button onClick={() => {
+                                    FollowHandler(u.id)
+                                }}>Follow</button>
+                        }
+
+                    </div>
+
+                </div>
+                <div className={s.content}>
+                    <div className={s.nameAndStatus}>
+                        <div>{u.fullName}</div>
+                        <div>{u.status}</div>
+                    </div>
+                    <div className={s.location}>
+                        <div>{u.location.country}</div>
+                        <div>{u.location.city}</div>
+
+                    </div>
+                </div>
+            </div>)}
+            <button className={s.button}>Show more</button>
         </div>
     );
 };
--- src/redax/usersReduser.ts
+++ src/redax/usersReduser.ts
@@ -1,5 +1,3 @@
-import {v1} from "uuid";
-
 
 export type UsersPageType = {
     id: string,
@@ -17,63 +15,63 @@ type ActionsType = ReturnType<typeof FollowAC> | ReturnType<typeof UnFollowAC> |
 
 export  type IninitialStateType = typeof initialState
 let initialState = {
-    users: [
-        {
-            id: v1(),
-            followed: true,
-            fullName: 'Alexander',
-            status: 'I am very well',
-            location: {city: 'Vileyka', country: 'Belarus'},
-        },
-        {
-            id: v1(),
-            followed: true,
-            fullName: 'John',
-            status: 'Feeling great!',
-            location: {city: 'New York', country: 'United States'}
-        },
-        {
-            id: v1(),
-            followed: true,
-            fullName: 'Emma',
-            status: 'Enjoying life',
-            location: {city: 'London', country: 'United Kingdom'}
-        },
-        {
-            id: v1(),
-            followed: true,
-            fullName: 'Luisa',
-            status: 'Feeling positive',
-            location: {city: 'Paris', country: 'France'}
-        },
-        {
-            id: v1(),
-            followed: true,
-            fullName: 'Mohammed',
-            status: 'Excited for the future',
-            location: {city: 'Dubai', country: 'United Arab Emirates'}
-        },
-        {
-            id: v1(),
-            followed: true,
-            fullName: 'Sofia',
-            status: 'Grateful for everything',
-            location: {city: 'Rome', country: 'Italy'}
-        },
-        {
-            id: v1(),
-            followed: true,
-            fullName: 'Hiroshi',
-            status: 'Energized and motivated',
-            location: {city: 'Tokyo', country: 'Japan'}
-        },
-        {
-            id: v1(),
-            followed: true,
-            fullName: 'Elena',
-            status: 'Finding joy in little things',
-            location: {city: 'Moscow', country: 'Russia'}
-        }
+     users: [
+    //     {
+    //         id: v1(),
+    //         followed: true,
+    //         fullName: 'Alexander',
+    //         status: 'I am very well',
+    //         location: {city: 'Vileyka', country: 'Belarus'},
+    //     },
+    //     {
+    //         id: v1(),
+    //         followed: true,
+    //         fullName: 'John',
+    //         status: 'Feeling great!',
+    //         location: {city: 'New York', country: 'United States'}
+    //     },
+    //     {
+    //         id: v1(),
+    //         followed: true,
+    //         fullName: 'Emma',
+    //         status: 'Enjoying life',
+    //         location: {city: 'London', country: 'United Kingdom'}
+    //     },
+    //     {
+    //         id: v1(),
+    //         followed: true,
+    //         fullName: 'Luisa',
+    //         status: 'Feeling positive',
+    //         location: {city: 'Paris', country: 'France'}
+    //     },
+    //     {
+    //         id: v1(),
+    //         followed: true,
+    //         fullName: 'Mohammed',
+    //         status: 'Excited for the future',
+    //         location: {city: 'Dubai', country: 'United Arab Emirates'}
+    //     },
+    //     {
+    //         id: v1(),
+    //         followed: true,
+    //         fullName: 'Sofia',
+    //         status: 'Grateful for everything',
+    //         location: {city: 'Rome', country: 'Italy'}
+    //     },
+    //     {
+    //         id: v1(),
+    //         followed: true,
+    //         fullName: 'Hiroshi',
+    //         status: 'Energized and motivated',
+    //         location: {city: 'Tokyo', country: 'Japan'}
+    //     },
+    //     {
+    //         id: v1(),
+    //         followed: true,
+    //         fullName: 'Elena',
+    //         status: 'Finding joy in little things',
+    //         location: {city: 'Moscow', country: 'Russia'}
+    //     }
     ] as Array<UsersPageType>,
 }
 export const FollowAC = (id: string) => {
@@ -98,6 +96,7 @@ export const SetUsersAC = (users: any) => {
 export const usersReduser = (state: IninitialStateType = initialState, action: ActionsType): IninitialStateType => {
     switch (action.type) {
         case 'FOLLOW': {
+
             return {
                 ...state, users: state.users.map(u => u.id === action.id ?
                     {...u, followed: true} : u)
